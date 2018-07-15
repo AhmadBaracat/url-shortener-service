15def8a09389ee3bc519dc6e5dfae08f4a5fbd7e
Baracat <baracat@aucegypt.edu>
2018-07-15 23:47:40 +0100

    Add tests for get_shortened_url endpoint

--- tests/test_urlshortenerservice.py
+++ tests/test_urlshortenerservice.py
@@ -1,4 +1,6 @@
-from urlshortenerservice import app
+import fakeredis
+
+from urlshortenerservice import create_app
 
 SHORTEN_URL_ROUTE = '/shorten_url'
 
@@ -6,7 +8,18 @@ SHORTEN_URL_ROUTE = '/shorten_url'
 class TestUrlShortenerService:
     @classmethod
     def setup_class(cls):
-        cls.url_shortener_service = app.test_client()
+        cls.r = fakeredis.FakeStrictRedis()
+        cls.url_shortener_service = create_app(cls.r).test_client()
+
+    def test_get_shortened_url_should_respond_with_404_if_key_is_not_in_datastore(self):
+        response = self.url_shortener_service.get('key')
+        assert response.status_code == 404
+
+    def test_get_shortened_url_should_redirect_to_original_url(self):
+        self.r.set('key', 'http://www.google.com')
+        response = self.url_shortener_service.get('key')
+        assert response.status_code == 302
+        assert response.headers.get("Location") == 'http://www.google.com'
 
     def test_shorten_url_should_respond_with_400_if_header_content_type_is_not_set(self):
         response = self.url_shortener_service.post(SHORTEN_URL_ROUTE)
