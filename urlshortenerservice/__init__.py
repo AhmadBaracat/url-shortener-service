e5e2d45e6ff0857d6539552712a617dc6f27be9e
Baracat <baracat@aucegypt.edu>
2018-07-15 21:10:26 +0100

    Refactor shorten_url request validation logic

--- urlshortenerservice/__init__.py
+++ urlshortenerservice/__init__.py
@@ -18,20 +18,29 @@ def get_shortened_url(shortened_url):
 
 @app.route('/shorten_url', methods=['POST'])
 def shorten_url():
+    is_valid, error_response = is_valid_post_request(request)
+
+    if not is_valid:
+        return error_response
+
+    return jsonify({"shortened_url": "placeholder"}), 201
+
+
+def is_valid_post_request(request):
     # Make sure that the request header was set correctly and
     # that the request contains json body
     if not request.is_json or request.get_json(False, True, True) is None:
-        return generate_error_msg("Your request should contain valid json body")
+        return False, generate_error_msg("Your request should contain valid json body")
 
     url = request.get_json().get('url', '')
 
     if url == '':
-        return generate_error_msg("Please provide a url to be shortened")
+        return False, generate_error_msg("Please provide a url to be shortened")
 
     if not validators.url(url):
-        return generate_error_msg("Please provide a valid url")
+        return False, generate_error_msg("Please provide a valid url")
 
-    return jsonify({"shortened_url": "placeholder"}), 201
+    return True, None
 
 
 def generate_error_msg(msg):
